0e71d07459860ba25ceb93c9c4dd3eb4703ae106
Evaggelos Balaskas <evaggelos [_at_] balaskas [_dot_] gr>
2019-09-01 18:43:58 +0300

    Tag Plugin for FlatPress v1.1

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # flatpress_tag_plugin
 Tag Plugin for FlatPress v1.1
+
+developed originally by Pier Angelo Vendrame
+https://www.pierov.org/2011/08/01/plugin-tag-v25-flatpress/
+
+Modified to work with php7 flatpress
+
+https://github.com/flatpressblog/flatpress
---
+++ tag/lang/lang.it-it.php
@@ -0,0 +1,22 @@
+<?php
+	$lang['plugin']['tag']=array(
+		'tag_s'=>'Tag: ',
+		'title_pre'=>'Tag: ',
+
+		'widget'=>'Tag Cloud',
+		'oneentry' => 'Un post',
+		'entry'=>' post',
+		'entries'=>' post',
+		'notags'=>'Non c\'è nessun tag.',
+
+		'related'=>'Post Correlati',
+		'norelated'=>'Non abbiamo trovato alcun post correlato.',
+		'error'=>'Errore',
+	);
+	$lang['plugin']['tag']['errors']=array (
+		-1	=> 'Questo plugin necessita di BBCode per essere abilitato. Forse i plugin sono stati abilitati nell\'ordine sbagliato.'
+	);
+	$lang['admin']['plugin']['tag']=array(
+		'tag_pl'=>'Plugin Tag',
+		'remove'=>'Rimuovi',
+	);
